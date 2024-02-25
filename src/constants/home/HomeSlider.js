5b3fcb0bf070bb8f70a4355748bcaa2b9cd01b64
ansh1423 <anshyadav42495@gmail.com>
2024-02-26 01:02:44 +0530

    first commit

---
+++ src/constants/home/HomeSlider.js
@@ -0,0 +1,11 @@
+export const HomeSlider = [
+    {
+        key:1,
+        image:"https://admin.mochishoes.com/banner/1703137860DESKTOP-BANNER_MOCHI_PARTY_DECMEBER_WOMEN_2.jpg"
+    },
+    {
+       key:2,
+       image:"https://admin.mochishoes.com/banner/1703137767DESKTOP-BANNER_MOCHI_PARTY_DECMEBER_MEN_1.jpg"
+    }
+    
+]
